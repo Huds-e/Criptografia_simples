810b574b32a9a7abe30643a872bf023062ef78ed
Hudson <hudsongenteboa70@gmail.com>
2023-11-06 19:31:12 -0300

    mudança interação / começo de encriptação

--- criptografia.c
+++ criptografia.c
@@ -1,56 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
-void help(char* exe_name) {
-    printf("How to use:\n");
-    printf("\t%s [txt_file_name] [/e] [/d]\n", exe_name);
-    printf("\t/e\tEnters the encrypt mode. (don't use with /d)\n");
-    printf("\t/d\tEnters the decrypt mode. (don't use with /e)\n");
-    printf("\t[txt_file_name]\tIt's the file that will be encrypted/decrypted.\n");
-}
+int get_line(FILE* file, char* buffer);
+void criptografy(char* buffer, int size, int key, FILE* file);
 
-int get_line(FILE* fd, char* buffer);
+int main() {
+    char file_name[50];
+    char operation;
+    int key;
+    
+    printf("Name of the file: ");
+    fgets(file_name, 50, stdin);
+    file_name[strlen(file_name) - 1] = '\0';
 
-int main(int argc, char* argv[]) {
+    FILE* fd = fopen(file_name, "r+");
+    FILE* answer = fopen("decrypt.txt", "w");
 
-    if(argc == 2) {
-        if(strcmp(argv[1], "help") == 0) {
-            help(argv[0]);
-        }else {
-            printf("Error! type '%s help' for useful information.\n", argv[0]);
+    if(fd == NULL || answer == NULL) return 1;
+
+    printf("Operation letters\n\tencrypt --> e\n\tdecrypt --> d\n--> ");
+    scanf("%c", &operation);
+
+    printf("Type the key: ");
+    scanf("%i", &key);
+
+
+    while(!feof(fd)) {
+        char line[500] = {0};
+        int i = get_line(fd, line);
+        if(operation == 'e') {
+            criptografy(line, i, key, answer);
+        }else if(operation == 'd') {
+            criptografy(line, i, -key, answer);
         }
-    }else if(argc == 3) {
-        FILE * fd = fopen(argv[1], "r+");
-        if(fd == NULL) printf("Error! File not found.\n");
-        
-        else{
-            char buffer[500];
-            int i = get_line(fd, buffer);
+    }
 
-            if(strcmp(argv[2], "/e") == 0) {
-                // encrypt
-            } else if(strcmp(argv[2], "/d") == 0) {
-                // decrypt
-            } else {
-                printf("Error! type '%s help' for useful information.\n", argv[0]);
-            }
-        }        
-    }else printf("Error! type '%s help' for useful information.\n", argv[0]);
+    fclose(fd);
+    fclose(answer);
+
+    system("pause");
 
     return 0;
 }
 
 // get_line != fread
 // fread reads more than one line, get_line reads only one line a time.
-int get_line(FILE* fd, char* buffer) {
+int get_line(FILE* file, char* buffer) {
     char c; 
     int i = 0;
 
-    while((c = getc(fd)) != '\n' && i <= strlen(buffer)) {
+    while((c = getc(file)) != '\n' && i <= strlen(buffer)) {
         buffer[i++] = c;
     }
 
     return i;
+}
+
+void criptografy(char* buffer, int size, int key, FILE* file) {
+    while(size >= 0) {
+        printf("%c\n", buffer[size-1]);
+        buffer[size-1] = buffer[size-1] + key;
+        size--;
+        printf("%c\n", buffer[size-1]);
+    }
+
+    fwrite(buffer, sizeof(char), strlen(buffer), file);
 }
